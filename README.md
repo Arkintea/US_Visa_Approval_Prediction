d396ab2bcaaf2200fb792901c2899f461f3608ac
Akintayo Akinpelu <Akinpeluakintayo@outlook.com>
2024-03-06 14:54:36 +0000

    app, web interface

--- README.md
+++ README.md
@@ -43,4 +43,4 @@ export MONGODB_URL="mongodb+srv://<username>:<password>...."
 export AWS_ACCESS_KEY_ID=<AWS_ACCESS_KEY_ID>
 
 export AWS_SECRET_ACCESS_KEY=<AWS_SECRET_ACCESS_KEY>
-```
+```bash
--- us_visa/pipline/training_pipeline.py
+++ us_visa/pipline/training_pipeline.py
@@ -7,20 +7,22 @@ from us_visa.components.data_validation import DataValidation
 from us_visa.components.data_transformation import DataTransformation
 from us_visa.components.model_trainer import ModelTrainer
 from us_visa.components.model_evaluation import ModelEvaluation
-
+from us_visa.components.model_pusher import ModelPusher
 
 from us_visa.entity.config_entity import (DataIngestionConfig,
                                           DataValidationConfig,
                                           DataTransformationConfig,
                                           ModelTrainerConfig,
-                                          ModelEvaluationConfig)
+                                          ModelEvaluationConfig,
+                                          ModelPusherConfig)
                                           
 
 from us_visa.entity.artifact_entity import (DataIngestionArtifact,
                                             DataValidationArtifact,
                                             DataTransformationArtifact,
                                             ModelTrainerArtifact,
-                                            ModelEvaluationArtifact)
+                                            ModelEvaluationArtifact,
+                                            ModelPusherArtifact)
 
 
 
@@ -30,7 +32,8 @@ class TrainPipeline:
         self.data_validation_config = DataValidationConfig()
         self.data_transformation_config = DataTransformationConfig()
         self.model_trainer_config = ModelTrainerConfig()
-        self.model_evaluation_config = ModelEvaluationConfig
+        self.model_evaluation_config = ModelEvaluationConfig()
+        self.model_pusher_config = ModelPusherConfig()
 
 
     
@@ -79,8 +82,9 @@ class TrainPipeline:
         
 
 
-    def start_data_transformation(self, data_ingestion_artifact: DataIngestionArtifact, 
-                                  data_validation_artifact: DataValidationArtifact) -> DataTransformationArtifact:
+    
+
+    def start_data_transformation(self, data_ingestion_artifact: DataIngestionArtifact, data_validation_artifact: DataValidationArtifact) -> DataTransformationArtifact:
         """
         This method of TrainPipeline class is responsible for starting data transformation component
         """
@@ -89,50 +93,59 @@ class TrainPipeline:
                                                      data_transformation_config=self.data_transformation_config,
                                                      data_validation_artifact=data_validation_artifact)
             data_transformation_artifact = data_transformation.initiate_data_transformation()
-
             return data_transformation_artifact
         except Exception as e:
             raise USvisaException(e, sys)
         
+
     
-    def start_model_trainer(self, data_ingestion_artifact: DataIngestionArtifact,
-                                data_validation_artifact: DataValidationArtifact,
-                                data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
+    def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting model training
+        """
         try:
-
-            model_trainer = ModelTrainer(model_trainer_config = self.model_trainer_config, 
-                                         data_ingestion_artifact = data_ingestion_artifact, 
-                                         data_validation_artifact = data_validation_artifact, 
-                                         data_transformation_artifact = data_transformation_artifact)
+            model_trainer = ModelTrainer(data_transformation_artifact=data_transformation_artifact,
+                                         model_trainer_config=self.model_trainer_config
+                                         )
             model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
 
-            return model_trainer_artifact        
         except Exception as e:
             raise USvisaException(e, sys)
-      
-        pass
-
+        
+    
 
     def start_model_evaluation(self, data_ingestion_artifact: DataIngestionArtifact,
-                                data_validation_artifact: DataValidationArtifact,
-                                data_transformation_artifact: DataTransformationArtifact,
-                                model_trainer_artifact: ModelTrainerArtifact) -> ModelEvaluationArtifact:
+                               model_trainer_artifact: ModelTrainerArtifact) -> ModelEvaluationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting modle evaluation
+        """
         try:
-
-            model_evaluation = ModelEvaluation(model_evaluation_config = self.model_evaluation_config, 
-                                         data_ingestion_artifact = data_ingestion_artifact, 
-                                         data_validation_artifact = data_validation_artifact, 
-                                         data_transformation_artifact = data_transformation_artifact, 
-                                         model_trainer_artifact = model_trainer_artifact)
+            model_evaluation = ModelEvaluation(model_eval_config=self.model_evaluation_config,
+                                               data_ingestion_artifact=data_ingestion_artifact,
+                                               model_trainer_artifact=model_trainer_artifact)
             model_evaluation_artifact = model_evaluation.initiate_model_evaluation()
             return model_evaluation_artifact
-        
         except Exception as e:
             raise USvisaException(e, sys)
         
-        
 
     
+
+    def start_model_pusher(self, model_evaluation_artifact: ModelEvaluationArtifact) -> ModelPusherArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting model pushing
+        """
+        try:
+            model_pusher = ModelPusher(model_evaluation_artifact=model_evaluation_artifact,
+                                       model_pusher_config=self.model_pusher_config
+                                       )
+            model_pusher_artifact = model_pusher.initiate_model_pusher()
+            return model_pusher_artifact
+        except Exception as e:
+            raise USvisaException(e, sys)
+
+            
     def run_pipeline(self, ) -> None:
         """
         This method of TrainPipeline class is responsible for running complete pipeline
@@ -140,15 +153,16 @@ class TrainPipeline:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
-            data_transformation_artifact = self.start_data_transformation(data_ingestion_artifact=data_ingestion_artifact, 
-                                                                          data_validation_artifact=data_validation_artifact)
-            model_trainer_artifact = self.start_model_trainer(data_ingestion_artifact=data_ingestion_artifact, 
-                                                              data_validation_artifact=data_validation_artifact, 
-                                                              data_transformation_artifact=data_transformation_artifact)
-            model_evaluation_artifact = self.start_model_evaluation(data_ingestion_artifact=data_ingestion_artifact, 
-                                                              data_validation_artifact=data_validation_artifact, 
-                                                              data_transformation_artifact=data_transformation_artifact, 
-                                                              model_trainer_artifact=model_trainer_artifact)
+            data_transformation_artifact = self.start_data_transformation(
+                data_ingestion_artifact=data_ingestion_artifact, data_validation_artifact=data_validation_artifact)
+            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+            model_evaluation_artifact = self.start_model_evaluation(data_ingestion_artifact=data_ingestion_artifact,
+                                                                    model_trainer_artifact=model_trainer_artifact)
+            
+            if not model_evaluation_artifact.is_model_accepted:
+                logging.info(f"Model not accepted.")
+                return None
+            model_pusher_artifact = self.start_model_pusher(model_evaluation_artifact=model_evaluation_artifact)
 
 
         
